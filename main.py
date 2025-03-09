140ba40d3d656090d6917b150693b8186a2ba76b
Geoffrey <glouasse@gmail.com>
2025-03-09 21:06:08 +0100

    :sparkles: feat: datastats class and documentation

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ import sys
 from loguru import logger
 from datetime import datetime
 from utils.webpage_generator import WebpageGenerator
-from utils.url_scrapper import UrlScraper
+from utils.urls_scrapper import UrlsScraper
 from utils.config_loader import Config
-from utils.datastats import DataStats
+from utils.datastats_utils import DataStats
+from google.cloud import logging as gcloud_logging
+
+# Initialize Google Cloud Logging
+client = gcloud_logging.Client()
+client.setup_logging()
 
 if __name__ == '__main__':
 
@@ -25,7 +30,7 @@ if __name__ == '__main__':
     ) 
     
     # Date utils variables
-    script_execution_datetime = datetime.now()
+    script_execution_start_time = datetime.now()
     
     logger.info(f'Scrapping {config.JOB_TO_SCRAP} jobs')
     
@@ -39,79 +44,34 @@ if __name__ == '__main__':
         webpage = webpage_generator.start(url=url_to_scrap)                    
     except Exception as e:
         logger.error(f"Error while trying to generate webpage: {e}")
-        raise e
-
+        sys.exit(1)
     # ------------------------------------------------------------------------------------------------------------------
     # Beautifulsoup urls scraping from webpage
     # ------------------------------------------------------------------------------------------------------------------
 
     try:
         logger.info('Scraping urls and generating jobs list...')
-        url_scrapper = UrlScraper(webpage=webpage, job_to_scrap=config.JOB_TO_SCRAP) 
+        url_scrapper = UrlsScraper(webpage=webpage, job_to_scrap=config.JOB_TO_SCRAP) 
         urls_list = url_scrapper.generate_urls_list()                              
-        jobs_list = url_scrapper.get_jobs_list()                               
+        scraped_jobs_list = url_scrapper.get_jobs_list()                           
     except Exception as e:
         logger.error(f"Error while scraping with BeautifulSoup: {e}")
-        raise e
+        sys.exit(1)
 
     # ------------------------------------------------------------------------------------------------------------------
-    # Interact with DataStats
+    # Interact with DataStats resources : generating files, statistics, uploading, etc.
     # ------------------------------------------------------------------------------------------------------------------
 
-    datastats = DataStats(
-        script_execution_datetime=script_execution_datetime, 
-        job_to_scrap=config.JOB_TO_SCRAP,
-        config=config
-        )
-    
-    datastats.start_workflow()
-    
-    #datastats.add_scraped_jobs_to_monhtly_list(bucket_name=bucket_name, jobs_list=jobs_list)
-
-    # TODO : Ajouter la logique pour :
-    # - Vérifier si le json est présent dans le bucket
-    # - Si oui, le récupérer, si non, le créer en code et l'upload
-    
-    """     # For statistic purpose
-        today = script_execution_datetime.strftime("%Y-%m-%d")
-        script_duration = str(datetime.now() - script_execution_datetime).split('.')[0]
-        jobs_scraped = len(jobs_list)
-        jobs_scraped_matched = len(df)
-        
-        # Ajouter la logique pour insérer ces valeurs dans une table pgsql
-        print(today)
-        print(job_to_scrap)
-        print(jobs_scraped)
-        print(jobs_scraped_matched)
-        print(script_duration)     
-        
-    
-    if len(urls_list) > 0:
-        try:
-            logger.info(f'Generating {job_to_scrap} JSON')
-            job_data = {
-                'date': formatted_date_time,
-                'job': {
-                    job_to_scrap: urls_list
-                }
-            }
-            json_data = json.dumps(job_data, indent=2)
-        except Exception as e:
-            logger.error(f'Error while generating JSON: {e}')
-            raise e
-        
-        try:
-            logger.info(f'uploading {daily_jobs_file_name} to {bucket_name}...')
-            gcp = GoogleUtils
-            gcp.upload_non_physical_file(
-                bucket_name=bucket_name,
-                data=json_data,
-                destination_blob_name=daily_jobs_file_name,
-                content_type='application/json'
+    try:
+        logger.info('Starting Datastats resources workflow...')
+        datastats = DataStats(
+            script_execution_start_time=script_execution_start_time, 
+            scraped_jobs_list=scraped_jobs_list,
+            matched_jobs_list=urls_list,
+            config=config
             )
-        except Exception as e:
-            logger.error(f'Error while uploading JSON: {e}')
-            raise e
-    else:
-        logger.warning(f'No jobs have been scraped and matched with {job_to_scrap}.')
-    """
+        
+        datastats.start_workflow()
+    except Exception as e:
+        logger.error(f"Error while interacting with Datastats resources: {e}")
+        sys.exit(1)
